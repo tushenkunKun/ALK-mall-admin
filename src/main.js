9a4d46245262aab24ba9b778e86e0fa0146fe27a
安丽坤 <anlk@yiche.com>
2022-07-11 18:47:59 +0800

    Login中添加form表单组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,76 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+// 导入需要的组件
+import {
+  ElButton,
+  ElContainer,
+  ElAside,
+  ElHeader,
+  ElMain,
+  ElFooter,
+  ElMenu,
+  ElSubmenu,
+  ElMenuItemGroup,
+  ElMenuItem,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElPopover,
+  ElTag,
+  ElCard,
+  ElTable,
+  ElTableColumn,
+  ElPagination,
+  ElDialog,
+  ElPopconfirm,
+  ElUpload,
+  ElLoading,
+  ElSelect,
+  ElOption,
+  ElRadioGroup,
+  ElRadio,
+  ElCascader,
+  ElCheckbox,
+  ElInputNumber,
+} from "element-plus";
+// 导入完整的组件css
+import 'element-plus/dist/index.css'
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(store).use(router);
+// 使用导入后的各个组件
+app
+  .use(ElButton)
+  .use(ElContainer)
+  .use(ElAside)
+  .use(ElHeader)
+  .use(ElMain)
+  .use(ElFooter)
+  .use(ElMenu)
+  .use(ElSubmenu)
+  .use(ElMenuItemGroup)
+  .use(ElMenuItem)
+  .use(ElForm)
+  .use(ElFormItem)
+  .use(ElInput)
+  .use(ElPopover)
+  .use(ElTag)
+  .use(ElCard)
+  .use(ElTable)
+  .use(ElTableColumn)
+  .use(ElPagination)
+  .use(ElDialog)
+  .use(ElPopconfirm)
+  .use(ElUpload)
+  .use(ElLoading)
+  .use(ElSelect)
+  .use(ElOption)
+  .use(ElRadioGroup)
+  .use(ElRadio)
+  .use(ElCascader)
+  .use(ElCheckbox)
+  .use(ElInputNumber);
+app.mount("#app");
---
+++ src/views/Login.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="login-body">
+    <div class="login-container">
+      <div class="head">
+        <img class="logo" src="https://s.weituibao.com/1582958061265/mlogo.png" />
+        <div class="name">
+          <div class="title">新蜂商城</div>
+          <div class="tips">Vue3.0 后台管理系统</div>
+        </div>
+      </div>
+      <el-form label-position="top" :model="ruleForm" class="login-form">
+        <el-form-item label="帐号" prop="username">
+          <el-input v-model="ruleForm.username" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="ruleForm.password" />
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+import { reactive, ref, toRefs } from "vue";
+export default {
+  name: "Login",
+  setup() {
+    const state = reactive({
+      ruleForm: {
+        username: "",
+        password: "",
+      },
+    });
+    return { ...toRefs(state) };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.login-body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: #fff;
+  /* background-image: linear-gradient(25deg, #077f7c, #3aa693, #5ecfaa, #7ffac2); */
+}
+.login-container {
+  width: 420px;
+  height: 500px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0px 21px 41px 0px rgba(0, 0, 0, 0.2);
+}
+.head {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 40px 0 20px 0;
+}
+.head img {
+  width: 100px;
+  height: 100px;
+  margin-right: 20px;
+}
+.head .title {
+  font-size: 28px;
+  color: #1baeae;
+  font-weight: bold;
+}
+.head .tips {
+  font-size: 12px;
+  color: #999;
+}
+.login-form {
+  width: 70%;
+  margin: 0 auto;
+}
+</style>
+<style>
+.el-form--label-top .el-form-item__label {
+  padding: 0;
+}
+.login-form .el-form-item {
+  margin-bottom: 12px;
+}
+</style>
